554928ff41d792a92d396e486efa96b2118ed215
Dulan Vishwajith <dulanvishwajithgunarathna@gmail.com>
2026-09-12 21:22:20 +0530

    Stage 4: add file system structures and API

---
+++ kernel/fs.c
@@ -0,0 +1,12 @@
+#include "fs.h"
+
+/*
+ * Stage 4 file system implementation.
+ *
+ * The actual filesystem operations will be added
+ * incrementally in the following steps.
+ */
+
+void fs_init(void)
+{
+}
